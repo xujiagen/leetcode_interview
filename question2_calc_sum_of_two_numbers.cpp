c6209cfe18332ed825ceea09673393fe4c0a8586
xujiagen <xjiagen@gmail.com>
2018-11-08 11:44:49 +0800

    leetcode第二题，最终提交版本

--- question2_calc_sum_of_two_numbers.cpp
+++ question2_calc_sum_of_two_numbers.cpp
@@ -59,9 +59,9 @@ class Solution
         if(l2==NULL) return l1;
         int num1,num2,num;
         int sum_of_num=0;
-        ListNode* point=NULL;
-        ListNode* first=l1;
-        if(l1)
+        ListNode* first=new ListNode(0);
+        ListNode* point=first;
+        while(l1)
         {
             num1=l1->val;
             num2=l2->val;
@@ -75,21 +75,72 @@ class Solution
             {
                 sum_of_num=0;
             }
-            ListNode* temp=new ListNode(num);
-            if(l1==first)
-            {
-                first=point=temp;
-            }
-            else
-            {
-                first=temp;
-                first->next=point;
-                point=temp;
+            
+                point->next=new ListNode(num);
+                point=point->next;
 
             }
             l1=l1->next;
             l2=l2->next;
         }
-        return first;
+        return first->next;
+    }
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+        ListNode* pHead=new ListNode(0);
+        ListNode *pCurrent=pHead;
+	   int overload=0;//是不是有进位；有进位为1；
+        while(l1||l2||overload)
+        {
+            int sum=(l1?l1->val:0)+(l2?l2->val:0)+overload;//某一位的值为l1/l2的值加上进位的值；
+            if(sum>=10)//如果这个和大于10
+            {
+                sum%=10;//更新这个值
+                overload=1;//并且产生进位；
+            }
+            else
+                overload=0;
+            pCurrent->next=new ListNode(sum);//将当前的值赋给第一位；也就是个位数；
+            pCurrent=pCurrent->next;//当前的位置往下进行；也就是十位数
+            if(l1) l1=l1->next;//l1链表往后遍历；
+            if(l2) l2=l2->next;//l2链表往后遍历；
+        }
+        return pHead->next;
+    }
+};
+
+//下面是最终提交版本
+class Solution
+{
+    public:
+    ListNode* addTwoNumbers(ListNode*l1,ListNode*l2)
+    {
+        int add_flag=0;
+        int num1,num2,sum;
+        ListNode *first=new ListNode(0);
+        ListNode *point=first;
+        while(l1||l2||add_flag)
+        {
+            sum=(l1?l1->val:0)+(l2?l2->val:0)+add_flag;
+            if(sum>9)
+            {
+                sum=sum%10;
+                add_flag=1;
+            }
+            else
+            {
+                add_flag=0;
+            }
+            point->next=new ListNode(sum);
+            point=point->next;
+            l1=l1?l1->next:l1;
+            l2=l2?l2->next:l2;
+            //上面这一行判断主要是这个链表可能已经计算完了，但是累加之后还有数
+        }
+        return first->next;
     }
 };
