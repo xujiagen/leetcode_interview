5b466559ff382062c308a4da207c5e55d676ece9
xujiagen <xujiagen@momenta.ai>
2019-10-05 16:48:28 +0800

    添加动态规划题

---
+++ 动态规划/02.机器人的移动方式.cpp
@@ -0,0 +1,16 @@
+/*
+一个二维的数组，比如说3行7列，判断一个机器人从左上的格子移动到右下的格子需要多少步
+*/ 
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Solution
+{
+    int whole_step(int rows,int cols)
+    {
+        
+    }
+}
